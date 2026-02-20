7ba3aa33ae877286a8b60e0f72cf0828528c647b
Rushika Anumula <rushikaanumula2006@gmail.com>
2026-02-20 08:23:33 +0530

    Update solution for mean-squared-error

--- mean-squared-error/mean-squared-error.py
+++ mean-squared-error/mean-squared-error.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 def mean_squared_error(y_pred, y_true):
-    y_pred=np.array(y_pred)
-    y_true=np.array(y_true)
-    if y_pred.shape!=y_true.shape:
-        return None
-    diff=(y_pred-y_true)**2
-    mse=np.mean(diff)
-    return float(mse)
+     y_pred = np.array(y_pred)
+     y_true = np.array(y_true)
+     if y_pred.shape != y_true.shape:
+         return None
+     diff = (y_pred - y_true) ** 2
+     mse = np.mean(diff)
+     return float(mse)
